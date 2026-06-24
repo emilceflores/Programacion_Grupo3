46019f0d1df89dcd62e76746ae3ab6a86f4f4105
jhoelmachicadoF1 <jhoel.machicado@ucb.edu.bo>
2026-06-24 13:47:53 -0400

    Update LibJhoel.h

--- PROYECTO03/LibJhoel.h
+++ PROYECTO03/LibJhoel.h
@@ -41,7 +41,7 @@ void menuLoginPersonal()
             archivoNuevo.write((char*)&adminPorDefecto, sizeof(Usuario));
             archivoNuevo.close();
             
-            system("cls"); // OPORTUNO: Limpia para mostrar el aviso de creación limpia
+            system("cls"); 
             cout << "=========================================================" << endl;
             cout << "[SISTEMA]: Archivo vacio. Cuenta de Administrador activa." << endl;
             cout << "USUARIO: admin   |   CONTRASENA: 123" << endl;
@@ -55,7 +55,6 @@ void menuLoginPersonal()
         verificarArchivo.close();
     }
 
-    // FLUJO NORMAL DEL LOGIN
     system("cls");
     cout << "=== CONTROL DE ACCESO  ===" << endl;
     cout << "=================================" << endl;
@@ -72,12 +71,271 @@ void menuLoginPersonal()
             if (u.activo == true && strcmp(u.login, usuarioInput) == 0 && strcmp(u.password, passwordInput) == 0) 
             {
                 ingreso = true;
-                system("cls"); // OPORTUNO: Limpia los datos tecleados y da una bienvenida limpia
+                system("cls"); 
                 cout << "\n[OK]: Acceso concedido. Bienvenido " << u.login << endl;
                 system("pause");
                 
                 if (u.rol == 1) 
                 {
+                    menuAdministrador(); 
+                } else if (u.rol == 2) 
+                {
+                    menuRecepcionista(); 
+                }
+            }
+        }
+        archivoUser.close();
+        
+        if (!ingreso) 
+        {
+            system("cls"); 
+            cout << "=== ERROR DE AUTENTICACION ===" << endl;
+            cout << "==============================" << endl;
+            cout << "\n[ERROR]: Usuario o contrasena incorrectos." << endl;
+            cout << "[AVISO]: Si es personal nuevo (Recepcionista), requiere registro previo del Administrador." << endl;
+            system("pause");
+        }
+    } 
+    else 
+    {
+        system("cls"); 
+        cout << "\n[ERROR]: No se pudo abrir el archivo de usuarios." << endl;
+        system("pause");
+    }
+}
+
+// SUBMENÚ DE CONSULTA 
+void menuConsultaClientes() 
+{
+    int opcConsulta = 0;
+    do 
+    {
+        system("cls");
+        cout << "=== SUBMENU DE CONSULTA PARA CLIENTES ===" << endl;
+        cout << "========================================" << endl;
+        cout << "\t1. Ver Mapa de Habitaciones en Tiempo Real" << endl;
+        cout << "\t2. Ver Catalogo de Productos y Precios" << endl;
+        cout << "\t3. Buscar Estado de Reserva por CI" << endl;
+        cout << "\t0. Volver al Menu Principal" << endl;
+        cout << "Seleccione una opcion: ";
+        cin >> opcConsulta;
+        
+        switch(opcConsulta) 
+        {
+            case 1:
+                verMapaHabitaciones();
+                system("pause");
+                break;
+                
+            case 2:
+                verCatalogoPrecios();
+                system("pause");
+                break;
+                
+            case 3:
+                buscarReservaPorCI();
+                system("pause");
+                break;
+                
+            case 0:
+                system("cls"); 
+                cout << "Regresando al menu principal..." << endl;
+                break;
+                
+            default:
+                if (opcConsulta != 0) 
+                {
+                    system("cls");
+                    cout << "Opcion no valida." << endl;
+                    system("pause");
+                }
+                break;
+        }
+    } 
+    while(opcConsulta != 0);
+}
+
+// MAPA DE HABITACIONES (ACTUALIZADO CON "EJECUTIVA / MATRIMONIAL")
+void verMapaHabitaciones() 
+{
+    Habitacion h;
+    ifstream archivo("HABITACIONES.BIN", ios::binary);
+    
+    // Matriz de 11 filas (pisos 1 al 10) y 9 columnas (habitaciones 1 al 8)
+    int hotelMatriz[11][9];
+    
+    // Inicializamos toda la matriz en -1 (espacio vacío)
+    for (int f = 1; f <= 10; f++) {
+        for (int c = 1; c <= 8; c++) {
+            hotelMatriz[f][c] = -1;
+        }
+    }
+    
+    int totalesLibres = 0;
+    int totalesOcupadas = 0;
+
+    system("cls");
+    cout << "=========================================================================================================" << endl;
+    cout << "                              CEASARS PALACE HOTEL - MAPA DE OCUPACION                                   " << endl;
+    cout << "=========================================================================================================" << endl;
+
+    if (archivo) 
+    {
+        while (archivo.read((char*)&h, sizeof(Habitacion))) 
+        {
+            if (h.activo == true) 
+            {
+                int piso = h.numero / 100;
+                int cuarto = h.numero % 100;
+
+                if (piso >= 1 && piso <= 10 && cuarto >= 1 && cuarto <= 8) 
+                {
+                    hotelMatriz[piso][cuarto] = h.estado;
+                }
+            }
+        }
+        archivo.close();
+        
+        cout << "PISO\t| H1\t| H2\t| H3\t| H4\t| H5\t| H6\t| H7\t| H8\t| CATEGORIA\t\t| PRECIO x NOCHE" << endl;
+        cout << "---------------------------------------------------------------------------------------------------------" << endl;
+        
+        for (int f = 10; f >= 1; f--) 
+        {
+            int libresPiso = 0;
+            int ocupadasPiso = 0;
+            
+            cout << "Piso " << f << "\t| ";
+            for (int c = 1; c <= 8; c++) 
+            {
+                if (hotelMatriz[f][c] == -1) 
+                {
+                    cout << " - \t| "; 
+                }
+                else if (hotelMatriz[f][c] == 0) 
+                {
+                    cout << "[L]\t| "; 
+                    libresPiso++;
+                    totalesLibres++;
+                }
+                else if (hotelMatriz[f][c] == 1) 
+                {
+                    cout << "[O]\t| "; 
+                    ocupadasPiso++;
+                    totalesOcupadas++;
+                }
+            }
+            
+            // Colocamos las etiquetas informativas actualizadas tal como en el informe
+            if (f == 10) {
+                cout << " Suite Presidencial\t| 700.00 Bs.";
+            }
+            else if (f >= 7 && f <= 9) {
+                cout << " Suite VIP\t\t| 400.00 Bs.";
+            }
+            else if (f >= 4 && f <= 6) {
+                cout << " Ejecutiva / Matrimonial| 200.00 - 250.00 Bs.";
+            }
+            else if (f >= 1 && f <= 3) {
+                cout << " Simple / Economica\t| 90.00 - 120.00 Bs.";
+            }
+            
+            cout << " (L: " << libresPiso << " / O: " << ocupadasPiso << ")" << endl;
+        }
+        
+        cout << "---------------------------------------------------------------------------------------------------------" << endl;
+        cout << "MONITOR -> TOTAL LIBRES: " << totalesLibres << " | TOTAL OCUPADAS: " << totalesOcupadas << endl;
+        cout << "Leyenda: [L] = Libre | [O] = Ocupada |  -  = Espacio inexistente (Habitacion mas grande)" << endl;
+        cout << "=========================================================================================================" << endl;
+    } 
+    else 
+    {
+        cout << "[ERROR]: No se pudo abrir o leer el archivo HABITACIONES.BIN" << endl;
+    }
+}
+
+// CATÁLOGO DE PRECIOS
+void verCatalogoPrecios() 
+{
+    Producto p;
+    ifstream archivo("PRODUCTOS.BIN", ios::binary);
+    
+    system("cls");
+    cout << "=== CATALOGO DE PRODUCTOS Y PRECIOS ===" << endl;
+    cout << "=======================================" << endl;
+    cout << "ID\tNombre\t\tPrecio\tStock" << endl;
+    cout << "---------------------------------------" << endl;
+    
+    if (archivo) 
+    {
+        bool tieneProductos = false;
+        while (archivo.read((char*)&p, sizeof(Producto))) 
+        {
+            if (p.activo == true) 
+            {
+                tieneProductos = true;
+                cout << p.idProducto << "\t" << p.nombreProd << "\t\t" << p.precioVenta << " Bs.\t" << p.stock << endl;
+            }
+        }
+        archivo.close();
+        
+        if (!tieneProductos) 
+        {
+            cout << "[AVISO]: No hay productos registrados en el catalogo." << endl;
+        }
+    } 
+    else 
+    {
+        cout << "[ERROR]: No se pudo abrir el archivo de inventario." << endl;
+    }
+}
+
+// BUSCAR RESERVA POR CI
+void buscarReservaPorCI() 
+{
+    int ciBuscar; 
+    Reserva r;
+    bool encontro = false;
+    
+    system("cls");
+    cout << "=== BUSCAR ESTADO DE RESERVA POR CI ===" << endl;
+    cout << "=======================================" << endl;
+    cout << "Ingrese el CI del cliente: ";
+    cin >> ciBuscar;
+    
+    ifstream archivoRes("RESERVAS.BIN", ios::binary);
+    
+    if (archivoRes) 
+    {
+        system("cls"); 
+        
+        while (archivoRes.read((char*)&r, sizeof(Reserva))) 
+        {
+            if (r.ciHuesped == ciBuscar && r.activo == true)
+            {
+                cout << "=== RESERVA ENCONTRADA ===" << endl;
+                cout << "---------------------------------------" << endl;
+                cout << "Codigo Reserva: " << r.idReserva << endl;
+                cout << "Habitacion asignada: " << r.numHabitacion << endl;
+                cout << "Fecha de ingreso: " << r.fechaIngreso.dia << "/" << r.fechaIngreso.mes << "/" << r.fechaIngreso.anio << endl;
+                cout << "Dias de estadia: " << r.diasEstadia << endl;
+                cout << "---------------------------------------" << endl;
+                encontro = true;
+            }
+        }
+        archivoRes.close();
+        
+        if (!encontro) 
+        {
+            cout << "=== BUSQUEDA FINALIZADA ===" << endl;
+            cout << "\n[Aviso]: No se encontro ninguna reserva activa para el CI: " << ciBuscar << endl;
+        }
+    } 
+    else 
+    {
+        system("cls");
+        cout << "[ERROR]: No se pudo abrir el archivo de reservas." << endl;
+    }
+}                {
                     menuAdministrador(); 
                 } else if (u.rol == 2) 
                 {
@@ -112,7 +370,7 @@ void menuConsultaClientes()
     do 
     {
         system("cls");
-        cout << "=== SUBMENU DE CONSULTA PARA CLIENTES ===" << endl;
+        cout << "=== CONSULTA PARA CLIENTES ===" << endl;
         cout << "========================================" << endl;
         cout << "\t1. Ver Mapa de Habitaciones en Tiempo Real" << endl;
         cout << "\t2. Ver Catalogo de Productos y Precios" << endl;
